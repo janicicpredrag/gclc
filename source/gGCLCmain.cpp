2063b4f11145cff9f47762e624a343dfa77d9ae9
vadgoblin@arch <vadgoblin@github.com>
2026-08-05 09:33:24 +0200

    force light theme

--- source/gGCLCmain.cpp
+++ source/gGCLCmain.cpp
@@ -1,6 +1,7 @@
 #include "GUI/MainWindow.h"
 #include <QApplication>
 #include <locale.h>
+#include <QStyle>
 
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "en_GB");
@@ -8,6 +9,24 @@ int main(int argc, char *argv[]) {
   srand((unsigned)time(NULL));
 
   QApplication a(argc, argv);
+
+  QPalette lightPalette;
+  lightPalette.setColor(QPalette::Window, QColor(240, 240, 240));
+  lightPalette.setColor(QPalette::WindowText, Qt::black);
+  lightPalette.setColor(QPalette::Base, Qt::white);
+  lightPalette.setColor(QPalette::AlternateBase, QColor(233, 233, 233));
+  lightPalette.setColor(QPalette::ToolTipBase, Qt::white);
+  lightPalette.setColor(QPalette::ToolTipText, Qt::black);
+  lightPalette.setColor(QPalette::Text, Qt::black);
+  lightPalette.setColor(QPalette::Button, QColor(240, 240, 240));
+  lightPalette.setColor(QPalette::ButtonText, Qt::black);
+  lightPalette.setColor(QPalette::BrightText, Qt::red);
+  lightPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+  lightPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+  lightPalette.setColor(QPalette::HighlightedText, Qt::white);
+
+  QApplication::setPalette(lightPalette);
+
   MainWindow w;
   w.show();
 
